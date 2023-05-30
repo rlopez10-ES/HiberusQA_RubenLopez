98ff0b3ca3bfd7268f417460422eeef8c45936bc
rlopez10-ES <rlopez10.es@gmail.com>
2023-05-30 10:20:51 +0200

    Commit

--- EJ_RestAssured/src/test/java/runner/CucumberRunnerTest.java
+++ EJ_RestAssured/src/test/java/runner/CucumberRunnerTest.java
@@ -8,21 +8,20 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    //publish = true,
     publish = true,
     plugin = {
             "pretty",
             //"usage",
             "json:target/surefire-reports/cucumber.json",
             //"junit:target/cucumber-reports/Cucumber.xml",
-            "html:target/cucumber-html-report"
+            //"html:target/cucumber-html-report.html"
     },
     glue = {
-            "stepdefs",
-            "support"
+            "/stepdefs",
+            "/support"
     },
     features = {
-            "resources/features"
+            "src/test/resources/features"
     }
 )
 public class CucumberRunnerTest {
--- EJ_RestAssured_SwagerAPI/src/test/java/Support/Hook.java
+++ EJ_RestAssured_SwagerAPI/src/test/java/Support/Hook.java
@@ -1,9 +1,9 @@
-package support;
+package Support;
 
 import io.cucumber.java.Before;
 import io.restassured.RestAssured;
 
-public class Hooks {
+public class Hook {
     @Before
     public void before() {
         RestAssured.baseURI = "https://petstore.swagger.io/v2/";
